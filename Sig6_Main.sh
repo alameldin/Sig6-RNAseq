60824ecaa8b27e7136a1e7618dab6bfca7e29545
alameldin <hussienfotoh@gmail.com>
2018-11-07 14:35:51 -0500

    Uploading the main script

---
+++ Sig6_Main.sh
@@ -0,0 +1,93 @@
+## Define the path of your raw data and your scripts
+data_path=$"/mnt/scratch/hussien/Sig6/data"
+script_path=$"/mnt/scratch/hussien/Sig6/scripts"
+out_path=$"/mnt/scratch/hussien/Sig6/output"
+genome_path=$"/mnt/scratch/hussien/Sig6/genome"
+#####################################
+#Running the fastQC again
+ ### script name fastQC.sb
+#!/bin/bash --login
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=8G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hussien@msu.edu
+
+module load FastQC/0.11.7-Java-1.8.0_162
+
+cd /mnt/scratch/hussien/Sig6/data
+fastqc -f fastq -noextract ${sample}
+
+# to run the script 
+for S in *.fastq; do sbatch  --output=${out_path}/$S.out --export=sample=$S ${script_path}/fastqc.sb; done
+# the results output was generated in the same as input data
+# moving the fasrtQC output to a specific folder in the output 
+mkdir ${out_path}/fastqc
+cd ${out_path}/fastqc
+mv *.html ${out_path}/fastqc
+mv *.zip ${out_path}/fastqc
+#####################
+# there atre some over represetation of some adapter like sequences in some samples 
+###################
+# Running Trimmomatic
+###################
+#!/bin/bash --login
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=8G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hussien@msu.edu
+ Trimmomatic/0.38-Java-1.8.0_162
+ 
+ java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.38.jar SE -phred33 ${sample} ${sample}.out ILLUMINACLIP:/mnt/research/common-data/Bio/Trimmomatic/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+ 
+ ### to run the trimmomatic
+ for S in *.fastq; do sbatch  --output=${out_path}/$S.out --export=sample=$S ${script_path}/trimmomatic.sb; done
+ 
+ mkdir trimmomatic_output
+ cd trimmomatic_output/
+ mv ../*.out .
+ 
+ 
+  # rerun the fastQC to make sure everytinig is ok (no adapters)
+  
+for S in *.out; do sbatch  --output=${out_path}/$S.output --export=sample=$S ${script_path}/fastqc.sb; done
+mkdir ${out_path}/FastQC-after-trimmomatic
+cd mkdir ${out_path}/FastQC-after-trimmomatic
+mv ${data_path}/trimmomatic_output/*.html .
+mv ${data_path}/trimmomatic_output/*.zip .
+
+###### the data looks good (No adapters)
+## Running the STAR fo mapping 
+##Downloading the genome data:
+wget https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_chromosome_files/TAIR10_chr_all.fas
+wget https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_gff3/TAIR10_GFF3_genes.gff
+
+-running STAR aligner
+#1-convert gff3 to gtf file
+
+module load cufflinks
+gffread Cmos.gff3  -T -o Cmos.gtf
+sed -i 's/Chr/CHROMOSOME/g' TAIR10_gtf_genes.gtf > TAIR10_gtf_genes_corID.gtf
+# for indexing
+mkdir ${genome_path}/STAR_index
+module load STAR/2.6.0c
+
+STAR --runThreadN 3 --runMode genomeGenerate --genomeDir ${genome_path}/STAR_index --genomeFastaFiles ${genome_path}/TAIR10_chr_all.fas --sjdbGTFfeatureExon ${genome_path}/TAIR10_gtf_genes_corID.gtf 
+
+##Running the alignment 
+
+STAR --runThreadN 1 \ # can be increased if sufficient computational power is available
+--genomeDir ${ REF_DIR }/ STARindex / \
+--readFilesIn raw_reads / ERR458493 . fastq .gz , raw_reads / ERR458494 . fastq .gz ,
+raw_reads / ERR458495 . fastq .gz , raw_reads / ERR458496 . fastq .gz , raw_reads /
+ERR458497 . fastq .gz , raw_reads / ERR458498 . fastq .gz , raw_reads / ERR458499 .
+fastq .gz \
+--outFileNamePrefix alignment_STAR / WT_1_ \ #/path/to/output/dir/prefix
+--outFilterMultimapNmax 1 \ # only reads with 1 match in the reference will be returned as aligned
+--outReadsUnmapped Fastx \ # will generate an extra output file with the unaligned reads
+--twopassMode Basic\ # STAR will perform mapping , then extract novel junctions which will be inserted into the genome index which will then be used to re - map all reads
+--outSAMtype BAM SortedByCoordinate Unsorted \ #Mapping reads and generating unsorted and coordinate-sorted BAM files, BAM conversion and coordinate-sorting while mapping
+--alignIntronMin 21 --alignIntronMax 6000 \
+--quantMode GeneCounts \ #STAR will count number reads per gene while mapping, A read is counted if it overlaps (1nt or more) one and only one gene, The counts coincide with those produced by htseq-count with default parameters, STAR outputs read counts per gene into ReadsPerGene.out.tab file
+
